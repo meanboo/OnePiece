ed819ad145eee07730269535cc0bc210ed95c993
Sunbowl <meanboo@163.com>
2024-08-07 00:10:36 +0800

    test

---
+++ Os/os.py
@@ -0,0 +1,24 @@
+import os
+
+class OsFunc():
+    def __init__(self):
+        super().__init__()    
+    
+    def sizeof_fmt(num, suffix='B'):
+        for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+            if abs(num) < 1024.0:
+                return "%3.1f%s%s" % (num, unit, suffix)
+            num /= 1024.0
+        return "%.1f%s%s" % (num, 'Yi', suffix)
+    
+    def get_folder_size(self, directory):
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(directory):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                try:
+                    size = os.path.getsize(fp)
+                    total_size += size
+                except OSError:
+                    pass
+        return self.sizeof_fmt(total_size)
--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from UIConfig.onePiece import *
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import os
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QFileDialog, QMessageBox, QApplication
+from PyQt5.uic import loadUi
+from Os.os import *
+ 
 
+ 
+class MyApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        loadUi('UIConfig/onePiece.ui', self)  # 加载使用PyDesigner设计的UI文件
+        self.choseFloderPushButton.clicked.connect(self.choose_folder)
+ 
+    def choose_folder(self):
+        directory = QFileDialog.getExistingDirectory(self, "Choose Directory")
+        if directory:
+            folder_size = OsFunc.get_folder_size(self, directory)
+            QMessageBox.information(self, "Folder Size", f"The size of the folder is: {folder_size}")
+ 
+
+ 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+    app = QApplication([])
+    window = MyApp()
+    window.show()
+    app.exec_()
